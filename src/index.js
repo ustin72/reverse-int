873c1afb0afcbe11521afc508c1402348a6e5a39
ustin72 <ustin@polymir.by>
2022-07-01 20:43:21 +0300

    feat: change index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse (n) { 
+    let str = String(n);
+    if(n < 0) str = str.slice(1);
+      
+    let strRev = [];
+    let j = str.length - 1;
+
+    for (let i = 0; i<= str.length - 1; i++) {
+        strRev[j] = str[i];
+        j--;
+    }
+
+    return Number(strRev.join(''));
+};
